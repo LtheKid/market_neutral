b70826ec6899ee4ff531d130c9ab693f1423341f
lthekid <lesterkoh92@gmail.com>
2021-01-31 15:19:35 +0800

    oanda integration pending

---
+++ oanda.py
@@ -0,0 +1,47 @@
+import configparser  # 1 
+import oandapy as opy  # 2
+
+config = configparser.ConfigParser()  # 3
+config.read('oanda.cfg')  # 4
+
+oanda = opy.API(environment='practice',
+                access_token=config['oanda']['access_token'])  # 5
+
+import pandas as pd  # 6
+
+data = oanda.get_history(instrument='EUR_USD',  # our instrument
+                         start='2016-12-08',  # start data
+                         end='2016-12-10',  # end date
+                         granularity='M1')  # minute bars  # 7
+
+df = pd.DataFrame(data['candles']).set_index('time')  # 8
+
+df.index = pd.DatetimeIndex(df.index)  # 9
+
+df.info() # 10
+
+import numpy as np  # 11
+
+df['returns'] = np.log(df['closeAsk'] / df['closeAsk'].shift(1))  # 12
+
+cols = []  # 13
+
+for momentum in [15, 30, 60, 120]:  # 14
+    col = 'position_%s' % momentum  # 15
+    df[col] = np.sign(df['returns'].rolling(momentum).mean())  # 16
+    cols.append(col)  # 17
+
+%matplotlib inline
+import seaborn as sns; sns.set()  # 18
+
+strats = ['returns']  # 19
+
+for col in cols:  # 20
+    strat = 'strategy_%s' % col.split('_')[1]  # 21
+    df[strat] = df[col].shift(1) * df['returns']  # 22
+    strats.append(strat)  # 23
+
+df[strats].dropna().cumsum().apply(np.exp).plot()  # 24
+
+
+
