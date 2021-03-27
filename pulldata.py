bd5e86e5a924bc3d3afabd8850ec934cefa6a28c
lthekid <lesterkoh92@gmail.com>
2021-03-27 19:17:56 +0800

    update pulldata

--- pulldata.py
+++ pulldata.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pandas_datareader import get_data_yahoo as yh
 
-df = yh(['GME','AAPL','TSLA','TLT','SPY','QQQ','UGL','GLD','TMF')['Close']
+df = yh(['GME','AAPL','TSLA','TLT','SPY','QQQ','UGL','GLD','TMF'])['Close']
 df.to_csv("data.csv")
 
 
